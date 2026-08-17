716904c7490703c728c13d26f74426d8e05976c9
Devan Urag Singh <devanuragsingh@example.com>
2026-08-18 01:06:15 +0530

    fix cors for vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -3,25 +3,37 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.routes import router
 
+
 app = FastAPI(
     title="Multi-Agent AI Research Assistant"
 )
 
+
+# --------------------------------------------------
 # CORS Configuration
+# --------------------------------------------------
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://multi-agent-ai-research-assistant-4rd67pj1d.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+# --------------------------------------------------
+# API Routes
+# --------------------------------------------------
 
-# Register Routes
 app.include_router(router)
 
+
+# --------------------------------------------------
+# Health Check
+# --------------------------------------------------
 
 @app.get("/")
 def home():
